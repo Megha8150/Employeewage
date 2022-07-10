1624df16d31d57eeacf44bb302ab3ed978fcb669
Megha8150 <meghananayak436@gmail.com>
2022-07-10 23:23:09 +0530

    Calculated ParttimeWage

--- AssignmentDay3/EmployeeWage.cs
+++ AssignmentDay3/EmployeeWage.cs
@@ -12,24 +12,30 @@ namespace AssignmentDay3
         public static void employee()
         {
             int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 2;
             int WAGE_PER_HOUR = 20;
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
-            int empCheck = random.Next(0,2);
+            int empCheck = random.Next(0,3);
             if(empCheck==IS_FULL_TIME)
             {
                 empHrs = 8;
-                empWage = empHrs * WAGE_PER_HOUR;
-                Console.WriteLine(empWage);
+                
             }
-          
+
+          else if(empCheck==IS_PART_TIME)
+            {
+                empHrs = 4;
+            }
+
             else
             { 
                 Console.WriteLine("Employee is absent");
             }
+            empWage = empHrs * WAGE_PER_HOUR;
+            Console.WriteLine(empWage);
 
-         
 
         }
 
