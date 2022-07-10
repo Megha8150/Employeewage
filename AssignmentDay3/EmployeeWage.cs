23aff79e075c80fcf6ae14fe8cab429964b14b97
Megha8150 <meghananayak436@gmail.com>
2022-07-10 22:10:09 +0530

    Welcome message

---
+++ AssignmentDay3/EmployeeWage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentDay3
+{
+     class EmployeeWage
+    {
+        public static void employee()
+        {
+            Console.WriteLine("Welcome to Employee wage Computation program on master branch");
+        }
+    }
+}
